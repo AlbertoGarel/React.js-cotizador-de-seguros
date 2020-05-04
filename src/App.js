1c8ec3225e17efd33e94c3e28817378027e92f9f
AlbertoGarel <qmelias@gmail.com>
2020-05-04 17:32:32 +0200

    added styles to Formulario with Emotion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
-  return <Header titulo="Cotizador de seguros"/>;
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de seguros" />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
+  );
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Formulario = () => {
+  return (
+    <form>
+      <Campo>
+        <Label>Marca</Label>
+        <Select>
+          <option value="">--Seleccione--</option>
+          <option value="">Americano</option>
+          <option value="">Europeo</option>
+          <option value="">Asiatico</option>
+        </Select>
+      </Campo>
+
+      <Campo>
+        <Label>Año</Label>
+        <Select>
+          <option value="">-- Seleccione --</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+        </Select>
+      </Campo>
+
+      <Campo>
+        <Label>Plan</Label>
+        <InputRadio type="radio" name="plan" value="basico" />
+        Básico
+        <InputRadio type="radio" name="plan" value="completo" />
+        Completo
+      </Campo>
+
+      <button type="button">Cotizar</button>
+    </form>
+  );
+};
+
+export default Formulario;
